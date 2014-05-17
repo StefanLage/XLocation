b0e2ed61d074e453637cf0180e70a91ecc9621a8
Stefan Lage <lagestfan@gmail.com>
2014-05-18 00:59:44 +0200

    New class to contain annotation + complete address components

---
+++ XLocation/Classes/LAnnotation.h
@@ -0,0 +1,24 @@
+//
+//  LAnnotation.h
+//  XLocation
+//
+//  Created by Stefan Lage on 17/05/14.
+//  Copyright (c) 2014 StefanLage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface LAnnotation : NSObject
+
+@property (nonatomic, strong) NSString          * city;
+@property (nonatomic, strong) NSString          * country;
+@property (nonatomic, strong) NSString          * address;
+@property (nonatomic, strong) NSString          * zipCode;
+@property (nonatomic, strong) MKPointAnnotation * annotation;
+
+// Custom constructor
+-(id)initWithCity:(NSString*)ci country:(NSString*)co address:(NSString*)ad zipCode:(NSString*)zip location:(CLLocationCoordinate2D)loc;
+-(NSString*)concatAddress;
+
+@end
---
+++ XLocation/Classes/LAnnotation.m
@@ -0,0 +1,43 @@
+//
+//  LAnnotation.m
+//  XLocation
+//
+//  Created by Stefan Lage on 17/05/14.
+//  Copyright (c) 2014 StefanLage. All rights reserved.
+//
+
+#import "LAnnotation.h"
+
+@implementation LAnnotation
+
+-(id)initWithCity:(NSString*)ci country:(NSString*)co address:(NSString*)ad zipCode:(NSString*)zip location:(CLLocationCoordinate2D)loc{
+    self = [super init];
+    if(self){
+        _city                  = ci;
+        _country               = co;
+        _address               = ad;
+        _zipCode               = zip;
+        _annotation            = [[MKPointAnnotation alloc] init];
+        _annotation.coordinate = loc;
+        _annotation.title      = [NSString stringWithFormat:@"%@\n%@", _city, _country];
+    }
+    return self;
+}
+
+/*
+ * Concat all properties to get a complete address
+ */
+-(NSString*)concatAddress{
+    NSMutableString *result = [[NSMutableString alloc] init];
+    if(![self.address isEqualToString:@""])
+        [result appendString:[NSString stringWithFormat:@"%@, ", self.address]];
+    if(![self.zipCode isEqualToString:@""])
+        [result appendString:[NSString stringWithFormat:@"%@, ", self.zipCode]];
+    if(![self.city isEqualToString:@""])
+        [result appendString:[NSString stringWithFormat:@"%@, ", self.city]];
+    if(![self.country isEqualToString:@""])
+        [result appendString:[NSString stringWithFormat:@"%@", self.country]];
+    return result;
+}
+
+@end
