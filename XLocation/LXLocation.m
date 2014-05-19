5607767db3ae0242f1be864d973a739b52bbb68f
Stefan Lage <lagestfan@gmail.com>
2014-05-20 00:07:36 +0200

    Check if the xcodeproj file exist before saving file

--- XLocation/LXLocation.m
+++ XLocation/LXLocation.m
@@ -33,6 +33,7 @@ static NSString * const gMapUrlRequestFromCoordinates = @"http://maps.googleapis
 // Default Alert Messages
 static NSString * const defaultErrorDesc = @"Something went wrong!\nCannot find the address!";
 static NSString * const addressNotFound  = @"Address not found!";
+static NSString * const projectNotFound  = @"Xcode project not found at path: %@";
 static NSString * const noPointSelected  = @"Please select a location first!";
 static NSString * const generateDone     = @"File %@.gpx created! This one has been added to your current project in the Group named GPX.";
 
@@ -330,35 +331,42 @@ static NSString * const generateDone     = @"File %@.gpx created! This one has b
 }
 
 -(void)generateGpxWithFilename:(NSString*)filename address:(NSString*)ad city:(NSString*)ci postalCode:(NSString*)zip country:(NSString*)co lat:(NSNumber*)lat lng:(NSNumber*)lng{
-    // Be sure there is no space in the filename
-    filename = [filename stringByReplacingOccurrencesOfString:@" " withString:@"_"];
-    XCProject* project = [[XCProject alloc] initWithFilePath:self.currentXcodeProject];
-    XCGroup* group     = [project groupWithPathFromRoot:@"GPX"];
-    // GPX Group doesn't exist ?!
-    if(!group){
-        [[project rootGroup] addGroupWithPath:@"GPX"];
-        // Re-init it
-        group          = [project groupWithPathFromRoot:@"GPX"];
-        [project save];
+    // Project found ?
+    if([[NSFileManager defaultManager] fileExistsAtPath:self.currentXcodeProject]){
+        // Be sure there is no space in the filename
+        filename = [filename stringByReplacingOccurrencesOfString:@" " withString:@"_"];
+        XCProject* project = [[XCProject alloc] initWithFilePath:self.currentXcodeProject];
+        XCGroup* group     = [project groupWithPathFromRoot:@"GPX"];
+        // GPX Group doesn't exist ?!
+        if(!group){
+            [[project rootGroup] addGroupWithPath:@"GPX"];
+            // Re-init it
+            group          = [project groupWithPathFromRoot:@"GPX"];
+            [project save];
+        }
+        // Create a gpx file source
+        XCSourceFileDefinition* sourceFileDefinition = [[XCSourceFileDefinition alloc] initWithName:[NSString stringWithFormat:@"%@.gpx", filename]
+                                                                                               text:[NSString generateGpxWithFilename:filename
+                                                                                                                             latitude:lat
+                                                                                                                            longitude:lng
+                                                                                                                              address:ad
+                                                                                                                                 city:ci
+                                                                                                                              country:co
+                                                                                                                                  zip:zip]
+                                                                                               type:GPX];
+        // Add it to the current xcode project
+        [group addSourceFile:sourceFileDefinition];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [project save];
+            // Everything done -> show alert
+            [self showAlert:[NSString stringWithFormat:generateDone, filename]
+               withDelegate:self];
+        });
     }
-    // Create a gpx file source
-    XCSourceFileDefinition* sourceFileDefinition = [[XCSourceFileDefinition alloc] initWithName:[NSString stringWithFormat:@"%@.gpx", filename]
-                                                                                           text:[NSString generateGpxWithFilename:filename
-                                                                                                                         latitude:lat
-                                                                                                                        longitude:lng
-                                                                                                                          address:ad
-                                                                                                                             city:ci
-                                                                                                                          country:co
-                                                                                                                              zip:zip]
-                                                                                           type:GPX];
-    // Add it to the current xcode project
-    [group addSourceFile:sourceFileDefinition];
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [project save];
-        // Everything done -> show alert
-        [self showAlert:[NSString stringWithFormat:generateDone, filename]
+    else{
+        [self showAlert:[NSString stringWithFormat:projectNotFound, self.currentXcodeProject]
            withDelegate:self];
-    });
+    }
 }
 
 /*
